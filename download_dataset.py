1e732a8fa66a9bae9c235f21bfe4b85d629d609e
Dove <itspintus@gmail.com>
2026-09-12 09:37:41 +0200

    Add initial TF-IDF SVM clinical classifier with already splitted df

--- download_dataset.py
+++ download_dataset.py
@@ -1,22 +1,30 @@
-from pathlib import Path
+from pathlib import Path    # to set the file paths
 
-from datasets import load_dataset
+from datasets import load_dataset   # to import the datasets
 
 OUTPUT_DIR = Path("datasets")
 OUTPUT_DIR.mkdir(exist_ok=True)
 
 print("Downloading Medical Abstracts dataset...")
 
-dataset = load_dataset(
-    "TimSchopf/medical_abstracts",
-    split="train",
-)
+# splitting the dataset already into 2 csv 
+dataset = load_dataset("TimSchopf/medical_abstracts") 
+df_train = dataset["train"].to_pandas()
+df_test = dataset["test"].to_pandas()
 
-df = dataset.to_pandas()
 
-output_file = OUTPUT_DIR / "clinical_cases.csv"
-df.to_csv(output_file, index=False)
+train_file = OUTPUT_DIR / "clinical_cases_train.csv"
+test_file = OUTPUT_DIR / "clinical_cases_test.csv"
 
-print(f"Dataset saved to {output_file}")
-print(df.head())
-print(f"\nTotal samples: {len(df)}")
+# convert all to csv format 
+df_train.to_csv(train_file, index=False)
+df_test.to_csv(test_file, index=False)
+
+# check first rows 
+print(f"Datasets saved to {train_file} and {test_file}")
+print(df_train.head())
+print(df_test.head())
+
+# total samples for each 
+print(f"\nTrain dataframe total samples: {len(df_train)}")
+print(f"\nTest dataframe total samples : {len(df_test)}")
--- train_model.py
+++ train_model.py
@@ -1,31 +1,24 @@
 import pandas as pd 
 import joblib 
 
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score, classification_report
 
-# -- Load dataset --
 
-df = pd.read_csv("datasets/clinical_cases.csv")
+# loading the two datasets already splitted 
+df_train = pd.read_csv("datasets/clinical_cases_train.csv")
+df_test = pd.read_csv("datasets/clinical_cases_test.csv")
 
-X = df["medical_abstract"]
-y = df["condition_label"]
+# setting train/test input and target
+X_train = df_train["medical_abstract"]
+y_train = df_train["condition_label"]
+
+X_test = df_test["medical_abstract"]
+y_test = df_test["condition_label"]
 
 
-# -- Train / Test split --
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42,
-    stratify=y
-)
-
-# -- TF-IDF -- 
-
+# tf-idf vectorizer 
 vectorizer = TfidfVectorizer(
     stop_words="english",
     max_features=10000
@@ -34,14 +27,15 @@ vectorizer = TfidfVectorizer(
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
-# -- Train model --
 
+# setting up the svm classifier model 
 model = LinearSVC()
 
+# fitting the model 
 model.fit(X_train_vec, y_train)
 
-# -- Evaluation -- 
 
+# evalutation 
 predictions = model.predict(X_test_vec)
 
 print("\nAccuracy: ")
@@ -50,8 +44,8 @@ print(accuracy_score(y_test, predictions))
 print("\nClassification Report:\n")
 print(classification_report(y_test, predictions))
 
-# -- Save 
 
+# saving the model into the dir 
 joblib.dump(model, "models/clinical_classifier.joblib")
 joblib.dump(vectorizer, "models/tfidf_vectorizer.joblib")
 
